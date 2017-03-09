cc8d1b2c102ee63a1b7d5b5c5fde6649b7233190
Ivan Bashkirov <ivanbashkirov@protonmail.com>
2017-03-09 18:59:44 +0000

    added iterative search

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,6 +1,5 @@
 require_relative 'entry'
-require "csv"
-
+require 'csv'
 
 class AddressBook
   attr_accessor :entries
@@ -9,8 +8,15 @@ class AddressBook
     self.entries = []
   end
 
-  def binary_search(name)
+  def iterative_search(name)
+    @entries.each do |entry|
+      return entry if entry.name == name
+    end
 
+    return nil
+   end
+
+  def binary_search(name)
     lower = 0
     upper = @entries.length - 1
 
@@ -28,7 +34,7 @@ class AddressBook
       end
     end
 
-      return nil
+    nil
   end
 
   def import_from_csv(file_name)
@@ -36,22 +42,20 @@ class AddressBook
     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
     csv.each do |row|
       row_hash = row.to_hash
-      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+      add_entry(row_hash['name'], row_hash['phone_number'], row_hash['email'])
     end
   end
 
   def add_entry(name, phone_number, email)
     index = 0
     entries.each do |entry|
-      if name < entry.name
-        break
-      end
-      index+= 1
+      break if name < entry.name
+      index += 1
     end
     entries.insert(index, Entry.new(name, phone_number, email))
   end
 
   def remove_entry(name, phone_number, email)
-    entries.delete_if {|x| x.name == name and x.phone_number == phone_number and x.email == email}
+    entries.delete_if { |x| (x.name == name) && (x.phone_number == phone_number) && (x.email == email) }
   end
 end
